8ded93c95ed67fccef68f679311e5c6a5d07c022
James Norton <jamesnorton@gmail.com>
2016-03-14 19:58:03 -0400

    Got JDI .sourceNames working

--- src/debug_middleware/core.clj
+++ src/debug_middleware/core.clj
@@ -2,10 +2,15 @@
  (:require [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
            [clojure.tools.nrepl.transport :as t]
            [clojure.tools.nrepl.misc :refer [response-for returning]])
- (:import com.sun.jdi.Bootstrap))
+ (:import com.sun.jdi.Bootstrap
+          com.sun.jdi.request.BreakpointRequest))
  
 (def vm-manager-atom
  "Atom to hold the virtual machine manager"
+ (atom nil))
+ 
+(def vm-atom
+ "Atom to hold the virutal machine"
  (atom nil))
  
 (defn setup-debugger
@@ -33,20 +38,59 @@
        (when strata (println (.name ref-type) ":  Strata: " strata)))
      vm)))
      
+(defn find-location-in-ref-type
+ "Find a location within the given Reference type that matches the given
+ source file path and line number"
+ [ref-type line path]
+ (let [locs (.allLineLocations ref-type)]))
+
+(defn find-location
+ "Find the Location that matches the line number and source given"
+ [vm line path]
+ (let [ref-types (.allClasses vm)
+       cloj-ref-types (filter (fn [rt]
+                                (let [strata (set (.availableStrata rt))]
+                                   (contains? strata "Clojure")))
+                              ref-types)]))
+      
+
 ;; Returns a handler for operation.
 (defmulti handle-msg (fn [handler msg] (:op msg)))
 
-(defmethod handle-msg "set-breakpoint-bak"
-  [handler msg]
-  (println "SETTING BREAKPOINT")
-  (println msg)
-  (let [res (handler msg)]
-   (println res)
-   (assoc res :result "BREAKPOINT SET")))
-   
 (defmethod handle-msg "set-breakpoint"
   [handler {:keys [op session interrupt-id id transport] :as msg}]
   (println "SETTING BREAKPOINT")
+  (let [line (get "line" msg)
+        path (get "path" msg)
+        ref-types (.allClasses @vm-atom)]
+    (doseq [ref-type ref-types]
+      (let [name (.name ref-type)]
+        (println "NAME: " name) 
+        (when (or (re-find #"repl-test" name) 
+                  (re-find #"middleware" name))
+          (println ">>>>>>>>>>>>>>>> FOUND NAME: " name))
+        (try 
+          (let [source-names (.sourceNames ref-type "Clojure")]
+            (println "SOURCE NAMES: " source-names))
+          (catch Exception e
+            (println "Can't get source"))))))
+     
+    ;     cloj-ref-types (filter (fn [rt]
+    ;                                (let [strata (set (.availableStrata rt))]
+    ;                                   (contains? strata "Clojure")))
+    ;                             ref-types)]
+    ;  (some (fn [crt]
+    ;          (let [locs (.allLineLocations crt)]
+    ;                loc (some (fn [lc] (= line (.lineNumber lc)))
+    ;                          locs)]
+    ;             (when loc
+    ;               (let [evt-reg-mgr (.eventRequestManager vm)
+    ;                     breq (.createBreakPointRequest ev-req-mgr loc)
+    ;                     _ (.setSupsendPolicy breq (.SUSPEND_ALL com.sun.jdi.request.BreakpointRequest))
+    ;                     _ (.enable breq)
+    ;                     evt-queue (.eventQueue vm)
+    ;                     ]))))
+    
   (t/send transport (response-for msg :status "BREAKPOINT SET")))
 
 (defmethod handle-msg :default 
@@ -58,7 +102,7 @@
  "Lein middleware to handle debug requests." 
  [handler]
  ;; Initialize the debugger.
- (setup-debugger)
+ (reset! vm-atom (setup-debugger))
  
  (fn [msg] 
   (handle-msg handler msg)))
