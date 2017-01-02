2e68397eee84a322f27795e3ff4159dbe1a2c8f2
James Norton <jamesnorton@gmail.com>
2017-01-01 22:53:06 -0500

    Updated project.clj for clojars

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject debug-middleware "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject debug-middleware "0.1.1"
+  :description "nREPL middleware to support VS Code Continuum"
+  :url "https://github.com/indiejames/debug-middleware"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[compliment "0.2.7"]
-                 [cdt "1.2.6.3"]
+                 [org.clojars.jnorton/cdt "1.2.6.3"]
                  [cheshire "5.6.3"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/tools.logging "0.3.1"]
