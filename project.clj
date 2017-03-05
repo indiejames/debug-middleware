7665c608d2007345387a511a05ecb07fbd9df88e
James Norton <jamesnorton@gmail.com>
2017-03-05 17:34:15 -0500

    Updated version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject debug-middleware "0.4.0"
+(defproject debug-middleware "0.4.1"
   :description "nREPL middleware to support VS Code Continuum"
   :url "https://github.com/indiejames/debug-middleware"
   :license {:name "Eclipse Public License"
